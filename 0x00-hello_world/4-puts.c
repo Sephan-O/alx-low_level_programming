0dd361f182845593f3ab705fc07064215d799599
Sephan-O <ogolas.otn@gmail.com>
2022-03-10 12:02:36 -0800

    betty style puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
- 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
   char str1[] = "\"Programming is like building a multilingual puzzle";
-
   puts(str1);
-
   return 0;
 }
