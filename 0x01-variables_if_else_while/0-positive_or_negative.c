215edc5a3b83e665d5bcdd7f0a4ad148a5537792
Sephan-O <ogolas.otn@gmail.com>
2022-03-14 03:34:07 -0700

    betty style effery

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- *style description before main function
- */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
