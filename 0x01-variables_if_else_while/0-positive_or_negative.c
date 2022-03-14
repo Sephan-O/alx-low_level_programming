f02f9f0f55bcb18c25b7a3df7c543b47c0e6faf9
Sephan-O <ogolas.otn@gmail.com>
2022-03-14 07:31:58 -0700

    betty hell

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/*main -  more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - random number generator function
+ *
+ * Return: it returns zero
+ */
 int main(void)
 {
 int n;
