3e25cb095b0201ba4c89347bfc1749a7f9e983b9
Sephan-O <ogolas.otn@gmail.com>
2022-03-14 13:11:23 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-this function prints out the alphabet
+ *Return: It returns zero
+ */
+int main(void)
+{
+int count = 0;
+while (count <= 15)
+{
+printf("%x", count);
+count++;
+}
+printf("\n");
+return (0);
+}
