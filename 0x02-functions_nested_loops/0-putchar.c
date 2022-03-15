02aa1eafda5ddd8f21b4bd3b4f5963e4fadbf6d8
Sephan-O <ogolas.otn@gmail.com>
2022-03-15 11:31:19 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - this function prints _putchar
+ *
+ *Return: it returns zero
+ */
+int main(void)
+{
+char ch[] = "_putchar";
+printf("the chars printed are: %s\n", ch);
+return (0);
+}
