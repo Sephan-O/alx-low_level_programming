5fb2dbef3b6b30b1ad8746a43c1fd3db7fba82b4
Sephan-O <ogolas.otn@gmail.com>
2022-03-22 05:54:12 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - printing an array
+ * @a: the array to be printed
+ * @n: length of the array
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
