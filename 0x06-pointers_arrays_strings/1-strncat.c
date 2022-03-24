1680bb8d5d6a16654ef19296485ab10aab9f9627
Sephan-O <ogolas.otn@gmail.com>
2022-03-24 03:38:13 -0700

    concatenantion but with specified no. of chars to be appended

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_strcat - function that concatenates two strings
+ * *_strncat - function that concatenates two strings
  * @dest: the destination string
  * @src: the source string.
  * @n: This is the maximum number of characters to be appended.
@@ -12,21 +12,21 @@ char *_strncat(char *dest, char *src, int n)
 {
 return (strncat(dest, src, n));
 }
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+/* int main(void) */
+/* { */
+/*     char s1[98] = "Hello "; */
+/*     char s2[] = "World!\n"; */
+/*     char *ptr; */
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
+/*     printf("%s\n", s1); */
+/*     printf("%s", s2); */
+/*     ptr = _strncat(s1, s2, 1); */
+/*     printf("%s\n", s1); */
+/*     printf("%s", s2); */
+/*     printf("%s\n", ptr); */
+/*     ptr = _strncat(s1, s2, 1024); */
+/*     printf("%s", s1); */
+/*     printf("%s", s2); */
+/*     printf("%s", ptr); */
+/*     return (0); */
+/* } */
