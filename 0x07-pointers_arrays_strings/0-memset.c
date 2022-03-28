26809eceb45713bad49432cce04b7c7209157621
Sephan-O <ogolas.otn@gmail.com>
2022-03-28 05:13:10 -0700

    the main.h file has now also been added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+/* #include "main.h" */
 
 /**
  * *_memset - this function fills the memory with constant byte
@@ -10,42 +11,42 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-memset(s,b,n);
+memset(s, b, n);
 return (s);
 }
-/* void simple_print_buffer(char *buffer, unsigned int size) */
-/* { */
-/*         unsigned int i; */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
 
-/*         i = 0; */
-/*         while (i < size) */
-/*         { */
-/*                 if (i % 10) */
-/*                 { */
-/*                         printf(" "); */
-/*                 } */
-/*                 if (!(i % 10) && i) */
-/*                 { */
-/*                         printf("\n"); */
-/*                 } */
-/*                 printf("0x%02x", buffer[i]); */
-/*                 i++; */
-/*         } */
-/*         printf("\n"); */
-/* } */
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
 
-/* /\** */
-/*  * main - check the code */
-/*  * */
-/*  * Return: Always 0. */
-/*  *\/ */
-/* int main(void) */
-/* { */
-/*     char buffer[98] = {0x00}; */
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
 
-/*     simple_print_buffer(buffer, 98); */
-/*     _memset(buffer, 0x01, 95); */
-/*     printf("-------------------------------------------------\n"); */
-/*     simple_print_buffer(buffer, 98);     */
-/*     return (0); */
-/* } */
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
+}
