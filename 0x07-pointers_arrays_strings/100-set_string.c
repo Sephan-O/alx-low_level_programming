f36e73b7cd92bf6c26e6fb21788289f35032886f
Sephan-O <ogolas.otn@gmail.com>
2022-03-29 10:54:09 -0700

    pointer to pointer

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * set_string - A function that sets the value of a pointer to a char
- * @s: the source
- * @to: the destination
+ * set_string - A func that sets the value of a pointer to a char
+ * @s: source parameter.
+ * @to: target parameter.
+ * Return: voids don't return anything
  */
+
 void set_string(char **s, char *to)
+
 {
-*s = to;
+
+		*s = to;
+
 }
