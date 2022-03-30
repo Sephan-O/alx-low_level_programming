64d81131e5dcdb6b944e8d7fc2130b9dc6eb89e1
Sephan-O <ogolas.otn@gmail.com>
2022-03-30 13:07:55 -0700

    Finding the power of given number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a func that finds pow of x to y using recursion
+ * @x: the base number
+ * @y: the power number
+ * Return: returns an integer
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
