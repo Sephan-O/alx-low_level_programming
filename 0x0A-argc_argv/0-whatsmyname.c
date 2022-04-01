f32a9d2b399cfeaea99627ff4b5a8c2ce073b278
Sephan-O <ogolas.otn@gmail.com>
2022-04-01 12:52:41 -0700

    About argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - func that prints the program's name
+ * @argc: parameters supplied through the command line
+ * @argv: array of character pointer, which contains all the parameters.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n",argv[0]);
+return (0);
+}
